295c0155d728ca0437b54bdea083a4a46ba6fe14
Subicz <h163988@stud.u-szeged.hu>
2023-02-09 21:01:11 +0100

    Rendszerezés, navbar kirendelése

---
+++ Fungi/php/navbar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>navbar</title>
+</head>
+<body>
+
+<!-- itt csináljuk a menüsort hogy ne kelljen mindenhol-->
+    <div class="navbar">
+        <div class="icon">
+            <div class="menu">
+                <ul>
+                    <li><a href="../html/proba.html">About Fungi</a></li>
+
+                    <li><a href="propertyindex.php">PRRTY</a></li>
+
+                    <li><a href="data.php">DAS</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ Fungi/php/welcome.php
@@ -0,0 +1,36 @@
+<!--<! FUNGIIIII&ndash;&gt;-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Fungi</title>
+    <link rel="stylesheet" href="../css/welcome.css">
+    <link rel="icon" href="../img/icon.png"/>
+</head>
+<body>
+    
+<style>
+body {
+  background-image: url('../img/1.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+
+    <div class="main">
+        <?php
+            include_once "../php/navbar.php"
+        ?>
+
+        <div class="content">
+
+            <h1><span>FANTASTIC  <br> FUNGI</span> </h1>
+         
+			<h2></h2>
+            <h2 class="par">Német Tímea és Subicz Máté</h2>
+        </div>
+    </div>
+    
+</body>
+</html>
